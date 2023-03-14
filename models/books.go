d8c2dba1b407ee9bc83ce5b75a42f5d6258365ab
hzlnqodrey <hazlanmuhammadqodri3@gmail.com>
2023-03-14 14:01:49 +0700

    migrate books models

--- models/books.go
+++ models/books.go
@@ -4,9 +4,17 @@ import (
 	"gorm.io/gorm"
 )
 
+// Books Model
 type Books struct {
 	ID			uint	`gorm:"primary key; autoIncrement" json:"id"`
-	Author		*string	`json: "author"`
-	Title		*string `json: "title"`
-	Publisher 	*string `json: "publisher"`
+	Author		*string	`json:"author"`
+	Title		*string `json:"title"`
+	Publisher 	*string `json:"publisher"`
+}
+
+// Export Book
+func MigrateBooks(db *gorm.DB) error {
+	err := db.AutoMigrate(&Books{})
+
+	return err
 }
